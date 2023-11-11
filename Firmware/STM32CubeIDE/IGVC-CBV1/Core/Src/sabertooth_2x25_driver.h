f8d876e442b722de830e2cfc32bab424f4fd307f
eeden2 <95004697+eeden2@users.noreply.github.com>
2023-11-11 14:49:11 -0600

    UPDATE

--- Firmware/STM32CubeIDE/IGVC-CBV1/Core/Src/sabertooth_2x25_driver.h
+++ Firmware/STM32CubeIDE/IGVC-CBV1/Core/Src/sabertooth_2x25_driver.h
@@ -1,37 +1,7 @@
 /**********************************************************************************************
  *
  * Sabertooth_2x25_Driver.h
- * Version 2.0.2
- * 07 July, 2013
- *
- * Brad Hedges
- * H & H Enterprises
- * Stratford, Oklahoma
- *
- **********************************************************************************************
- **********************************************************************************************
- *
- * This is a multi-purpose device driver to operate the Dimension Engineering Sabertooth series
- * of H-Bridge motor controllers. These can be used to control permanent magnet DC motors
- * between 6 & 30 volts, up to 25 amps continuous and 50 amps surge.
- *
- * ____________________________+______________________________This driver works in the "Packetized Serial" mode, which must be set with the DIP_______________________
- * switches on the controller hardware. The driver defaults to operating only one
- * Sabertooth, using the lowest physical address of 128. The address is configurable in
- * the driver by assigning the address value to the constant "MOTOR_DRIVER_ADDRESS_x".
- *
- * The driver is written in the C language for Microchip PIC microcontrollers using CCS's
- * PIC-C Compiler, and was developed using version 4.120 of that compiler. I have also used it
- * with 32-bit PICs, using Microchip's C32 compiler.
- *
- * It should be easily convertible to other microcontrollers and compilers with minimal effort. I have
- * annotated the three lines which would have to be modified for a different compiler or
- * processor architecture.
- *
- * Dimension Engineering's web site is at:      www.dimensionengineering.com
- * CCS's web site is:                           www.ccsinfo.com
- * Microchip's web site:                        www.microchip.com
- *
+ * Version 1
  *********************************************************************************************/
 
 
@@ -115,14 +85,14 @@
 
     /* Use desired hardware address, per hardware DIP switch settings */
 
-    #define MOTOR_DRIVER_ADDRESS_1       128
+//    #define MOTOR_DRIVER_ADDRESS_1       128
     #define MOTOR_DRIVER_ADDRESS_2       129        //THIS IS ITTTTTTT!!!!!!!!!!!!!!
-    #define MOTOR_DRIVER_ADDRESS_3       130
-    #define MOTOR_DRIVER_ADDRESS_4       131
-    #define MOTOR_DRIVER_ADDRESS_5       132
-    #define MOTOR_DRIVER_ADDRESS_6       133
-    #define MOTOR_DRIVER_ADDRESS_7       134
-    #define MOTOR_DRIVER_ADDRESS_8       135
+//    #define MOTOR_DRIVER_ADDRESS_3       130
+//    #define MOTOR_DRIVER_ADDRESS_4       131
+//    #define MOTOR_DRIVER_ADDRESS_5       132
+//    #define MOTOR_DRIVER_ADDRESS_6       133
+//    #define MOTOR_DRIVER_ADDRESS_7       134
+//    #define MOTOR_DRIVER_ADDRESS_8       135
 
 
 /**********************************************************************************************
@@ -135,28 +105,15 @@
     #define DRIVE_FORWARD_1              0
     #define DRIVE_REVERSE_1              1
 
-    #define SET_MIN_VOLTAGE              2
-    #define SET_MAX_VOLTAGE              3
-
     #define DRIVE_FORWARD_2              4
     #define DRIVE_REVERSE_2              5
 
     #define DRIVE_MOTOR_1_7_BIT          6
     #define DRIVE_MOTOR_2_7_BIT          7
 
-    #define DRIVE_FORWARD_MIXED          8
-    #define DRIVE_REVERSE_MIXED          9
-    #define DRIVE_TURN_RIGHT_MIXED       10
-    #define DRIVE_TURN_LEFT_MIXED        11
-
     #define DRIVE_FWD_REV_7_BIT          12
     #define DRIVE_TURN_7_BIT             13
 
-    #define SET_SERIAL_TIMEOUT           14
-    #define SET_BAUD_RATE                15
-
-    #define SET_RAMPING_RATE             16
-    #define SET_DEADBAND                 17
 
     /* If any more commands are added by the manufacturer in the future, increase
      * COMMAND_HIGH_LIMIT to the highest command number in that new command set.
@@ -181,25 +138,6 @@
     #define BAUDRATE_38400               4
     #define DEFAULT_BAUDRATE             BAUDRATE_9600
 
-    #define MAX_MINIMUM_VOLTAGE          120
-    #define MIN_MINIMUM_VOLTAGE          0
-    #define DEFAULT_MINIMUM_VOLTAGE      0
-
-    #define MAX_MAXIMUM_VOLTAGE          128
-    #define MIN_MAXIMUM_VOLTAGE          0
-    #define DEFAULT_MAXIMUM_VOLTAGE      0
-
-    #define MAX_SERIAL_TIMEOUT           50
-    #define MIN_SERIAL_TIMEOUT           0
-    #define DEFAULT_SERIAL_TIMEOUT       0
-
-    #define MAX_RAMPING_VALUE            80
-    #define MIN_RAMPING_VALUE            0
-    #define DEFAULT_RAMPING_VALUE        1
-
-    #define MAX_DEADBAND                 127
-    #define MIN_DEADBAND                 0
-    #define DEFAULT_DEADBAND             3
 
 
 /**********************************************************************************************
@@ -212,22 +150,7 @@
  *
  *********************************************************************************************/
 
-
-    static uint8_t control_motors_sep ( uint8_t command1, uint8_t speed1, uint8_t command2, uint8_t speed2, uint8_t address );
-   
-    static uint8_t control_motors_mixed ( uint8_t command, uint8_t speed, uint8_t address );
-
-    static uint8_t set_minimum_controller_voltage ( uint8_t desired_minimum_voltage, uint8_t address );
-
-    static uint8_t set_maximum_controller_voltage ( uint8_t desired_maximum_voltage, uint8_t address );
-
-    static uint8_t set_serial_timeout ( uint8_t desired_timeout_period, uint8_t address );
-
     static uint8_t set_baudrate ( uint8_t desired_baudrate, uint8_t address );
-
-    static uint8_t set_ramping_rate ( uint8_t desired_ramping_rate, uint8_t address );
-
-    static uint8_t set_deadband_range ( uint8_t desired_deadband, uint8_t address );
    
     static void send_command ( uint8_t command, uint8_t value, uint8_t address );
    
